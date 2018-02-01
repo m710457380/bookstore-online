f230cee05345b3b84f51b74cc64d076c2cf7f803
lrf123qwe <lrf123qwe@qq.com>
2018-02-01 16:54:52 +0800

    first commit

---
+++ src/page/cart/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <header>购物车</header>
+        <div class="content">
+            <p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/page/me/Me.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <header>个人中心</header>
+        <div class="content">
+            <p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p><p>内容</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
